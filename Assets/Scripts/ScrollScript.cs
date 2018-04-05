cbbf56e668c8c7f13e81cbdc2b232d18c7f40bd9
dev001hajipro <dev001hajipro@gmail.com>
2018-04-05 19:34:23 +0900

    first commit

---
+++ Assets/Scripts/ScrollScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScript : MonoBehaviour {
+
+    public float scrollSpeed = 1f;
+    public float scrollOffset = 0f;
+    private Vector2 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * -scrollSpeed, scrollOffset);
+        transform.position = startPosition + (Vector2.right * newPosition);
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+﻿# Unity2D Scrolling Parallax
+
+
+## WebGL Demo
+https://dev001hajipro.github.io/TsumTsumLikeGame/
+
+
+## assets dependencies
+- [XML Texture Atlas Slicer](https://assetstore.unity.com/packages/tools/utilities/xml-texture-atlas-slicer-36103)
+- https://opengameart.org/content/background-elements
+- Simple Vector Icons
+
+## SE
+
+## 参考にしたサイト
